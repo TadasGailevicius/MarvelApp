95f33b5de718695d83dfaa51c80b07b5b950760e
Tadas <tadasgailevicius@yahoo.com>
2021-05-04 23:44:08 +0300

    Update Constants.kt

--- app/src/main/java/com/tedm/marvelapp/other/Constants.kt
+++ app/src/main/java/com/tedm/marvelapp/other/Constants.kt
@@ -2,6 +2,8 @@ package com.tedm.marvelapp.other
 
 object Constants {
 
-    const val BASE_URL = "https://gateway.marvel.com:443/v1/"
+    const val BASE_URL = "https://gateway.marvel.com:443/v1/public/"
+    const val API_KEY_PUBLIC = "6b05d8cf6b4ea02d23003f6df9c27600"
+    const val API_KEY_PRIVATE = "a195b697395fa1c564c2899a67a3a3cc52da80de"
     const val PAGE_SIZE = 20
 }
